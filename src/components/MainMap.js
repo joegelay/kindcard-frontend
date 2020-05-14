f632fcd179947f2e405a4ddfeb0e12fedaae2f79
joegelay <joegelay@gmail.com>
2020-05-14 15:24:58 -0600

    change marker color to gold

--- src/components/MainMap.js
+++ src/components/MainMap.js
@@ -1,5 +1,16 @@
 import React from 'react'
 import L from 'leaflet';
+import goldIcon from '../images/marker-icon-gold.png'
+import markerShadow from '../images/marker-shadow.png'
+
+const greenIcon = new L.Icon({
+  iconUrl: goldIcon,
+  shadowUrl: markerShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
 
 class Map extends React.Component {
   componentDidMount() {
@@ -21,7 +32,7 @@ class Map extends React.Component {
   updateMarkers(markersData) {
     this.layer.clearLayers();
     markersData.forEach(marker => {
-      L.marker(marker.latLng, { title: marker.title }).bindPopup("<b>Hello world!</b><br>I am a popup.").addTo(this.layer);
+      L.marker(marker.latLng, {icon: greenIcon}).bindPopup("<b>Hello world!</b><br>I am a popup.").addTo(this.layer);
     });
   }
 
