541031aef7dca7052c1b2a303d323f48f0a8ac13
joegelay <joegelay@gmail.com>
2020-05-14 15:04:29 -0600

    add popup to markers

--- src/components/MainMap.js
+++ src/components/MainMap.js
@@ -4,8 +4,8 @@ import L from 'leaflet';
 class Map extends React.Component {
   componentDidMount() {
     this.map = L.map('map', {
-      center: [49.8419, 24.0315],
-      zoom: 16,
+      center: [39.810, -30.988],
+      zoom: 2,
       layers: [
         L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
           attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -13,8 +13,7 @@ class Map extends React.Component {
       ]
     });
 
-    // this.marker = L.marker(this.props.markerPosition).addTo(this.map);
-
+  
     this.layer = L.layerGroup().addTo(this.map);
     this.updateMarkers(this.props.markersData);
   }
@@ -22,7 +21,7 @@ class Map extends React.Component {
   updateMarkers(markersData) {
     this.layer.clearLayers();
     markersData.forEach(marker => {
-      L.marker(marker.latLng, { title: marker.title }).addTo(this.layer);
+      L.marker(marker.latLng, { title: marker.title }).bindPopup("<b>Hello world!</b><br>I am a popup.").addTo(this.layer);
     });
   }
 
