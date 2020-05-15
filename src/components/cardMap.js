5126b87dc01ee378c2f52310fc02de30ba927d6c
joegelay <joegelay@gmail.com>
2020-05-15 14:20:13 -0600

    add react-router

---
+++ src/components/cardMap.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import L from 'leaflet';
+import customIcon from '../images/marker-icon-gold.png'
+import markerShadow from '../images/marker-shadow.png'
+
+const goldIcon = new L.Icon({
+  iconUrl: customIcon,
+  shadowUrl: markerShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+class Map extends React.Component {
+  componentDidMount() {
+    this.map = L.map('map', {
+      center: [19.810, 0],
+      zoom: 2,
+      layers: [
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }),
+      ]
+    });
+
+    this.layer = L.layerGroup().addTo(this.map);
+    this.updateMarkers(this.props.markersData);
+  }
+
+  componentDidUpdate({ markersData }) {
+    if (this.props.markersData !== markersData) {
+      this.updateMarkers(this.props.markersData);
+    }
+  }
+
+  updateMarkers(markersData) {
+    this.layer.clearLayers();
+    markersData.forEach(story => {
+      L.marker([story.lat, story.lng], {icon: goldIcon}).bindPopup(`
+      <h1>KindCard #${story.number}</h1><br><a href='http://localhost:4000/cards/${story.cardId}' target=_blank>Read stories</a>`)
+      .addTo(this.layer);
+    });
+  }
+  
+  render() {
+    return <div id="map"></div>
+  }
+}
+
+export default Map;
---
+++ src/components/cardPage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import MainMap from './MainMap'
+
+export default function cardPage(props) {
+      return (
+          <div>
+            <h1 id="brand">KindCard</h1>
+            <h2 id="tagline">Every Act Mapped.</h2>
+            <MainMap markersData={props.markersData} />
+            <div id="main-button-container">
+                <button className="main-button">Enter your card &rarr;</button> 
+                <button className="main-button">View random card &rarr;</button>
+            </div>
+          </div> 
+      );
+  }
+
+  
