39bfbc17a2516ece14ffacd2106f55efcc859a77
joegelay <joegelay@gmail.com>
2020-05-18 17:10:30 -0600

    add labels and label styles for card submission form

--- src/components/EntryForm.js
+++ src/components/EntryForm.js
@@ -12,19 +12,25 @@ export default function EntryForm() {
         <div id="formContainer">
             <h1 id="formHeader">SHARE YOUR STORY</h1>
             <form onSubmit={handleSubmit(onSubmit)} id="entryForm">
+                <label for="cardNumber">CARD NUMBER</label>
                 <input className="fieldset"
+                    id="cardNumber"
                     type="number" 
                     placeholder="000" 
                     name="cardNumber" 
                     ref={register({ required: true, minLength: 3 })} 
                 />
+                <label for="email">EMAIL</label>
                 <input className="fieldset"
+                    id="email"
                     type="email" 
                     placeholder="you@example.com" 
                     name="email" 
                     ref={register({ required: true })} 
                 />
+                <label for="story">HOW DID YOU RECEIVE YOUR CARD?</label>
                 <textarea className="fieldset"
+                    id="story"
                     placeholder="Share your story" 
                     name="story" 
                     ref={register({ required: true, minLength: 5 })} 
