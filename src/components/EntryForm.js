9450221196051662515fe0b652b2a511212e23c2
joegelay <joegelay@gmail.com>
2020-05-18 17:03:22 -0600

    style submit button of card entry form

--- src/components/EntryForm.js
+++ src/components/EntryForm.js
@@ -9,29 +9,33 @@ export default function EntryForm() {
     }
 
     return(
-        <form onSubmit={handleSubmit(onSubmit)} id="entryForm">
-            <input 
-                type="number" 
-                placeholder="000" 
-                name="cardNumber" 
-                ref={register({ required: true, minLength: 3 })} 
-            />
-            <input 
-                type="email" 
-                placeholder="you@example.com" 
-                name="email" 
-                ref={register({ required: true })} 
-            />
-            <textarea 
-                placeholder="Share your story" 
-                name="story" 
-                ref={register({ required: true, minLength: 8 })} 
-                form="entryForm"
-            ></textarea>
-            {errors.cardNumber && <p>Invalid card number</p>}
-            {errors.email && <p>Invalid email</p>}
-            {errors.story && <p>Invalid story</p>}
-            <input type="submit" />
-        </form>
+        <div id="formContainer">
+            <h1 id="formHeader">SHARE YOUR STORY</h1>
+            <form onSubmit={handleSubmit(onSubmit)} id="entryForm">
+                <input className="fieldset"
+                    type="number" 
+                    placeholder="000" 
+                    name="cardNumber" 
+                    ref={register({ required: true, minLength: 3 })} 
+                />
+                <input className="fieldset"
+                    type="email" 
+                    placeholder="you@example.com" 
+                    name="email" 
+                    ref={register({ required: true })} 
+                />
+                <textarea className="fieldset"
+                    placeholder="Share your story" 
+                    name="story" 
+                    ref={register({ required: true, minLength: 5 })} 
+                    form="entryForm"
+                ></textarea>
+                {errors.cardNumber && <p>Invalid card number</p>}
+                {errors.email && <p>Invalid email</p>}
+                {errors.story && <p>Invalid story</p>}
+                <input className="styledSubmit" type="submit" />
+            </form>
+        </div>
+        
     )
 }
