979f6db7c8828ee2439af0b3534791fb82f2b2fc
joegelay <joegelay@gmail.com>
2020-05-18 16:42:19 -0600

    add card number, email, and story textarea to form with validations

--- src/components/EntryForm.js
+++ src/components/EntryForm.js
@@ -9,9 +9,28 @@ export default function EntryForm() {
     }
 
     return(
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <input type="text" placeholder="Email" name="email" ref={register} />
-            <input type="password" placeholder="Password" name="password" ref={register}/>
+        <form onSubmit={handleSubmit(onSubmit)} id="entryForm">
+            <input 
+                type="number" 
+                placeholder="000" 
+                name="cardNumber" 
+                ref={register({ required: true, minLength: 3 })} 
+            />
+            <input 
+                type="email" 
+                placeholder="you@example.com" 
+                name="email" 
+                ref={register({ required: true })} 
+            />
+            <textarea 
+                placeholder="Share your story" 
+                name="story" 
+                ref={register({ required: true, minLength: 8 })} 
+                form="entryForm"
+            ></textarea>
+            {errors.cardNumber && <p>Invalid card number</p>}
+            {errors.email && <p>Invalid email</p>}
+            {errors.story && <p>Invalid story</p>}
             <input type="submit" />
         </form>
     )
