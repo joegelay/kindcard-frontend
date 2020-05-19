25dbbc2c15b7c7202585acb00511cde63f2a9157
joegelay <joegelay@gmail.com>
2020-05-18 21:51:55 -0600

    react-leaflet-geosearch added to form map

--- src/components/FormMap.js
+++ src/components/FormMap.js
@@ -1,17 +1,21 @@
 import React from 'react'
-import { Map, Marker, Popup, TileLayer } from "react-leaflet";
-// import { Icon } from "leaflet";
-// import customIcon from '../images/marker-icon-gold.png'
-// import markerShadow from '../images/marker-shadow.png'
+import { Map, Marker, Popup, TileLayer, withLeaflet } from "react-leaflet";
+import { Icon } from "leaflet";
+import customIcon from '../images/marker-icon-gold.png'
+import markerShadow from '../images/marker-shadow.png'
+import { SearchControl, OpenStreetMapProvider } from 'react-leaflet-geosearch'
 
-// const goldIcon = new Icon({
-//   iconUrl: customIcon,
-//   shadowUrl: markerShadow,
-//   iconSize: [25, 41],
-//   iconAnchor: [12, 41],
-//   popupAnchor: [1, -34],
-//   shadowSize: [41, 41]
-// });
+const goldIcon = new Icon({
+  iconUrl: customIcon,
+  shadowUrl: markerShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+const prov = OpenStreetMapProvider();
+const GeoSearchControlElement = withLeaflet(SearchControl);
 
 export default function FormMap() {
       return (
@@ -20,6 +24,22 @@ export default function FormMap() {
                 <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+                <GeoSearchControlElement 
+                    // style={'bar'}
+                    position={"topright"}
+                    provider={prov} 
+                    showMarker={true} 
+                    showPopup={false} 
+                    popupFormat={({ query, result }) => result.label} 
+                    maxMarkers={1}  
+                    retainZoomLevel= {false}  
+                    animateZoom={true} 
+                    autoClose={true}  
+                    searchLabel={'Enter location'} 
+                    keepResult={true} 
+                    markerIcon={goldIcon}
+                    className="custom-search-style"
                 />
              </Map>
       </div>
