8adab59b3fc4c6d84fefa7aaa1ea60fe5555de63
joegelay <joegelay@gmail.com>
2020-05-18 22:17:00 -0600

    use react-leaflet-search for card submission map

--- src/components/FormMap.js
+++ src/components/FormMap.js
@@ -1,47 +1,54 @@
-import React from 'react'
-import { Map, Marker, Popup, TileLayer, withLeaflet } from "react-leaflet";
-import { Icon } from "leaflet";
-import customIcon from '../images/marker-icon-gold.png'
-import markerShadow from '../images/marker-shadow.png'
-import { SearchControl, OpenStreetMapProvider } from 'react-leaflet-geosearch'
+import React, { Component } from "react";
+import { Map, TileLayer, Popup, withLeaflet } from "react-leaflet";
+import { ReactLeafletSearch } from "react-leaflet-search";
 
-const goldIcon = new Icon({
-  iconUrl: customIcon,
-  shadowUrl: markerShadow,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41]
-});
 
-const prov = OpenStreetMapProvider();
-const GeoSearchControlElement = withLeaflet(SearchControl);
-
-export default function FormMap() {
-      return (
-        <div className="form-map">
-            <Map center={[19.810, 0]} zoom={1}>
-                <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                />
-                <GeoSearchControlElement 
-                    // style={'bar'}
-                    position={"topright"}
-                    provider={prov} 
-                    showMarker={true} 
-                    showPopup={false} 
-                    popupFormat={({ query, result }) => result.label} 
-                    maxMarkers={1}  
-                    retainZoomLevel= {false}  
-                    animateZoom={true} 
-                    autoClose={true}  
-                    searchLabel={'Enter location'} 
-                    keepResult={true} 
-                    markerIcon={goldIcon}
-                    className="custom-search-style"
-                />
-             </Map>
-      </div>
-      );
+export default class FormMap extends Component {
+  
+  customPopup(SearchInfo) {
+    return (
+      <Popup>
+        <div>
+          <p>
+            latitude and longitude from search component:{" "}
+            {SearchInfo.latLng.toString().replace(",", " , ")}
+          </p>
+          <p>Info from search component: {SearchInfo.info}</p>
+          <p>
+            {SearchInfo.raw &&
+              SearchInfo.raw.place_id &&
+              JSON.stringify(SearchInfo.raw.place_id)}
+          </p>
+        </div>
+      </Popup>
+    );
   }
+
+  render() {
+    const ReactLeafletSearchComponent = withLeaflet(ReactLeafletSearch);
+
+    return (
+    <div className="form-map">
+      <Map
+        center={[19.810, 0]} zoom={1}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <ReactLeafletSearchComponent
+          position="topleft"
+          inputPlaceholder="Enter location"
+          search={[33.100745405144245, 46.48315429687501]}
+          showMarker={true}
+          zoom={5}
+          showPopup={true}
+          popUp={this.customPopup}
+          closeResultsOnClick={true}
+          openSearchOnLoad={false}
+        />
+      </Map>
+      </div>
+    );
+  }
+}
