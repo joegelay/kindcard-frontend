035abba65a3916283b8c0f5a972241f45862e766
joegelay <joegelay@gmail.com>
2020-05-21 10:48:38 -0600

    add random card number for 'view random card' navigation on homepage

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useRef} from 'react'
 import { Link } from "react-router-dom";
 import MainMap from './MainMap'
 import Header from './Header'
@@ -6,41 +6,35 @@ import Footer from './Footer'
 
 export default function Homepage(props) {
 
-    // const [cardData, setCardData] = useState([]);
-    // const [loading, setLoading] = useState(true);
-    
-    useEffect(() => {
-        fetch(`http://localhost:4000/cards/`)
-            .then(response => response.json())
-            .then(result => {
-                 console.log(result)
-            })
-            // .then(setLoading(false))
-
-            // return () => setLoading(true)
+    const cardNumbers = []
+    let randomCardNumber = ''
+     
+    props.markersData.forEach(marker => {
+        cardNumbers.push(marker.number)
     })
 
-    const randomId = '009'
+    const randomNumber = cardNumbers[Math.floor(Math.random()*cardNumbers.length)]
+    randomCardNumber = randomNumber
 
-      return (
-          <div className="App">
-            <Header />
-            <div>
-                <h1 id="brand">KindCard</h1>
-                <h2 id="tagline">Every Act Mapped.</h2>
-            </div>
-            <MainMap markersData={props.markersData} />
-            <div id="main-button-container">
-                <Link to='/share-your-story' className="main-button">
-                    Enter your card &rarr;
-                </Link>
-                <Link to={`/cards/${randomId}`} className="main-button">
-                    View random card &rarr;
-                </Link>
-            </div>
-            <Footer />
-          </div> 
-      );
+    return (
+        <div className="App">
+        <Header />
+        <div>
+            <h1 id="brand">KindCard</h1>
+            <h2 id="tagline">Every Act Mapped.</h2>
+        </div>
+        <MainMap markersData={props.markersData} />
+        <div id="main-button-container">
+            <Link to='/share-your-story' className="main-button">
+                Enter your card &rarr;
+            </Link>
+            <Link to={`/cards/${randomCardNumber}`} className="main-button">
+                View random card &rarr;
+            </Link>
+        </div>
+        <Footer />
+        </div> 
+    );
   }
 
   
