abf09e97b999d9ca7fed4748654fec77bfe9d2ec
joegelay <joegelay@gmail.com>
2020-05-21 20:02:47 -0600

    add signup blurb to login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -4,10 +4,15 @@ import Footer from './Footer'
 import LoginForm from './LoginForm'
 import CreateAccountForm from './CreateAccountForm'
 
+const signUpBlurb = `
+    Create an account to easily keep track of all your KindCards as they travel onward!
+`
+
 export default function LoginPage() {
     return (
         <div id="login-page" className="App">
             <SecondaryHeader />
+            <p id="sign-up-blurb">{signUpBlurb}</p>
             <div id="login-form-container">
             <LoginForm />
             <h1 id="or">OR</h1>
